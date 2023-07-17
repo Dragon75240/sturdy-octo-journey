effbd0b613a732112874a5b00dd5abc8316ea031
Dragon <stevyn034@gmail.com>
2023-07-17 16:29:25 -0600

    asdfggfade

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from discord import Webhook
 
 webhookURL = os.getenv('WEBHOOK_URL')
 
+async def main():
+    async with aiohttp.ClientSession() as session:
+        webhook = Webhook.from_url(webhookURL, session=session)
 
-with aiohttp.ClientSession() as session:
-    webhook = Webhook.from_url(webhookURL, session=session)
+        webhook.send("sad", username="sdasd")
 
-    webhook.send("sad", username="sdasd")
+main()
