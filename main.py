9ae80e2735242de4e88d2e1cadc45954c40b6c66
Dragon <stevyn034@gmail.com>
2023-07-17 17:04:07 -0600

    byrts bes

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ webhookURL = os.getenv('WEBHOOK_URL')
 
 webhook = dwh.Discord(url=webhookURL)
 
-for i in range(123):
-    webhook.post(content="@everyone")
+for i in range(2):
+    webhook.post(content="ping test ||@everyone||")
