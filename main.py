ded5d07f8cc42cf3f551fe8af711f251341a4bd1
Dragon <stevyn034@gmail.com>
2023-07-17 16:12:36 -0600

    asd

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import os
 from discord import Webhook
 
+webhookURL = os.getenv('WEBHOOK_URL')
 
+print(webhookURL)
+
+'''
 webhook = Webhook.from_url()
 
 webhook.send("sad", username="sdasd")
 
+'''
