b8407a200e8ec76a43f1294d3eda250cbd8f5a07
Dragon <stevyn034@gmail.com>
2023-07-17 16:27:33 -0600

    asDF

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ webhookURL = os.getenv('WEBHOOK_URL')
 
 async def foo():
     async with aiohttp.ClientSession() as session:
-        webhook = Webhook.from_url(webhookURL)
+        webhook = Webhook.from_url(webhookURL, session=session)
 
         webhook.send("sad", username="sdasd")
