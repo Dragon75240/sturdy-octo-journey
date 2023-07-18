2a464823647753d41bce6566c994343b425e0b4c
Dragon <stevyn034@gmail.com>
2023-07-18 12:51:34 -0600

    gfag

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import http.client
 import os
-from time import sleep
 import discordwebhook as dwh
 
 webhookURL = os.getenv('WEBHOOK_URL')
 
 webhook = dwh.Discord(url=webhookURL)
 
-for i in range(50):
-    webhook.post(content="@dragonning ")
+for i in range(5):
+    webhook.post(content="@Everyone")
